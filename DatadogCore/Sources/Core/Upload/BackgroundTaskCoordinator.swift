a2ed121db6c56fbdc773da3190c0aa5dfbc4d509
Nikolai Prokofev <nvprokofiev@gmail.com>
2024-06-11 00:38:54 -0400

    v3

--- DatadogCore/Sources/Core/Upload/BackgroundTaskCoordinator.swift
+++ DatadogCore/Sources/Core/Upload/BackgroundTaskCoordinator.swift
@@ -22,8 +22,8 @@ import DatadogInternal
 
 /// Bridge protocol that matches `UIApplication` interface for background tasks. Allows easier testablity.
 internal protocol UIKitAppBackgroundTaskCoordinator {
-    func beginBackgroundTask(expirationHandler handler: (@Sendable () -> Void)?) -> UIBackgroundTaskIdentifier
-    func endBackgroundTask(_ identifier: UIBackgroundTaskIdentifier)
+    nonisolated func beginBackgroundTask(expirationHandler handler: (@Sendable () -> Void)?) -> UIBackgroundTaskIdentifier
+    nonisolated func endBackgroundTask(_ identifier: UIBackgroundTaskIdentifier)
 }
 
 extension UIApplication: UIKitAppBackgroundTaskCoordinator {}
